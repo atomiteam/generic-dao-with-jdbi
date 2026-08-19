52ca90ea64ba2cc1effa59067c4d9465a2a3557f
Sami Altundag <samialtundag@gmail.com>
2026-08-19 08:30:32 +0300

    Preserve explicit String id accessors on Entity

--- src/main/java/org/atomiteam/jdbi/generic/dao/Entity.java
+++ src/main/java/org/atomiteam/jdbi/generic/dao/Entity.java
@@ -4,4 +4,14 @@ package org.atomiteam.jdbi.generic.dao;
  * Backward-compatible entity base using a String primary key.
  */
 public class Entity extends BaseEntity<String> {
+
+    @Override
+    public String getId() {
+        return super.getId();
+    }
+
+    @Override
+    public void setId(String id) {
+        super.setId(id);
+    }
 }
